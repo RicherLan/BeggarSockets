33066111eabc7e640dc0959ecfd549f879e1d69c
BeggarLan <1348270542@qq.com>
2023-07-04 16:14:09 +0800

    [update]android宏

---
+++ app/jni/Config.h
@@ -0,0 +1,19 @@
+/** 
+ * author: BeggarLan
+ * created on: 2023/7/4 13:47 
+ * description: 
+ */
+
+#ifndef BEGGARSOCKETS_CONFIG_H
+#define BEGGARSOCKETS_CONFIG_H
+
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+// android
+#ifndef ANDROID
+#define ANDROID
+#endif
+
+#endif //BEGGARSOCKETS_CONFIG_H
