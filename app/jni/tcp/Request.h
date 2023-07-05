bce308b0f160b33483c70bb8b223343db5f197cf
BeggarLan <1348270542@qq.com>
2023-07-05 14:53:16 +0800

    [feat]请求Request定义

---
+++ app/jni/tcp/Request.h
@@ -0,0 +1,46 @@
+/** 
+ * author: BeggarLan
+ * created on: 2023/7/5 10:42 
+ * description: 请求
+ */
+
+#ifndef BEGGARSOCKETS_REQUEST_H
+#define BEGGARSOCKETS_REQUEST_H
+
+#include "TcpConstant.h"
+
+class NativeByteBuffer;
+
+class Request {
+public:
+    Request(NativeByteBuffer *buffer, int32_t cmdId, uint32_t flags, uint64_t timeout, onSendCompleteFunc sendCompleteFunc);
+    ~Request();
+
+    // ***************** 必要的参数
+    // 请求的buffer
+    NativeByteBuffer *buffer;
+    // 命令
+    int32_t cmdId;
+    // 一些flag
+    uint32_t flags;
+    // 超时时间
+    uint64_t timeoutMs;
+    // 请求完成的回调
+    onSendCompleteFunc  sendCompleteFunc;
+
+    // ***************** other
+    // 请求开始的时候更新(包括retry)
+    uint64_t startTimeMs;
+    // 重试次数
+    int32_t retryCount;
+    // token
+    uint32_t connectionToken = 0;
+    // 标识消息的
+    int32_t  messageSeq = 0;
+
+    void onComplete(NativeByteBuffer *buffer, uint32_t code, string desc);
+    bool isTimeout(uint64_t currentTimeMs);
+    bool canRetry();
+};
+
+#endif //BEGGARSOCKETS_REQUEST_H
--- app/jni/tcp/TcpConstant.h
+++ app/jni/tcp/TcpConstant.h
@@ -8,6 +8,10 @@
 #define BEGGARSOCKETS_TCPCONSTANT_H
 
 #include <stdint.h>
+#include <string.h>
+#include <functional>
+
+class NativeByteBuffer
 
 /**
  * 所有的event
@@ -17,6 +21,18 @@ enum EventType {
     Pipe,        // 管道
     Fd,         // EventFd
     Timer,      // 定时器
+};
+
+/**
+ * 请求完成回调
+ */
+typedef std::function<void(NativeByteBuffer *buffer, uint32_t code, string desc)> onSendCompleteFunc
+
+// 请求的flag
+enum RequestFlag {
+    WithoutAck = 1,  // 没有回包
+    ReSend = 2,      // 重发
+    WithoutLogin = 3 // 可以在握手前发送
 };
 
 #endif //BEGGARSOCKETS_TCPCONSTANT_H
